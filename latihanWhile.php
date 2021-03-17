9a2a6a55dfb8dc9a16d36b7223250e5d9cbbb674
ramadan69 <dokumen.ramadan@gmail.com>
2021-03-17 16:52:10 +0700

    menambahkan file latihanwhile.php

---
+++ latihanWhile.php
@@ -0,0 +1,29 @@
+<?php
+$a = 1;
+while ($a<10){
+    echo $a;
+    $a++;
+}
+echo "<br>";
+?>
+
+<?php
+$n = 1;
+while ($n<10){
+    if ($n != 5 ) {
+        echo $n;}
+
+    $n++;
+}
+echo "<br>";
+?>
+
+<?php
+$m = 1;
+while ($m<10){
+    if ($m != 5 ) {
+    echo $m;}
+    $m = $m + 2;
+}
+echo "<br>";
+?>
